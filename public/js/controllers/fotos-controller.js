3830da429345a13af352772859f8ceaaff0c21a6
ramonqueiroga <ramon.araujo.queiroga@gmail.com>
2016-08-25 22:39:11 -0300

    subindo os primeiros arquivos do projeto

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,18 @@
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
+
+	$scope.fotos = [];
+
+	$http.get('v1/fotos').success(function(fotos) {
+		$scope.fotos = fotos
+	}).error(function(error) {
+		console.log(error)
+	});
+
+	/*var promise = $http.get('v1/fotos');
+	promise.then(function(retorno) {
+		$scope.fotos = retorno.data;
+	}).catch(function(error) {
+		console.log(error);
+	});*/
+
+});
---
+++ public/js/directives/minhas-diretivas.js
@@ -0,0 +1,25 @@
+angular.module('minhasDiretivas', []).directive('meuPainel', function() {
+	var ddo = {};
+	/*
+	 * A = Attribute (<div meu-painel>)
+	   E = Element (<meu-painel>)
+	 */
+	ddo.restrict = "AE";
+	ddo.scope = {
+		/** @ garante que o conteúdo informado para o parâmetro título em <meu-painel titulo="teste"> vai ser passado como String para o atributo titulo do scopo dessa diretiva */
+		titulo: '@'
+	}
+	/** 
+	transclude serve para informar ao Angular que queremos manter os elementos filhos que forem colocados dentro de nossa diretiva. Exemplo:
+	<meu-painel>
+		<p>teste</p>
+	</meu-painel>
+
+	Além disso precisamos informar dentro da diretiva qual o elemento em que os elementos filhos de nossa direta vai ficar.
+	Nesse caso colocamos ele para a div panel-body
+	*/
+	ddo.transclude = true;
+	ddo.templateUrl = 'js/directives/meu-painel.html'
+
+	return ddo;
+});
