433dbf712c3276ab9a12ef583fbda3ab9fc1573f
ramonqueiroga <ramon.araujo.queiroga@gmail.com>
2016-08-28 11:09:59 -0300

    Criando busca com filtro para as imagens. Adicionando as primeiras rotas, dividindo em parciais o html e melhorando a url com locationProvider.

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -2,17 +2,13 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 
 	$scope.fotos = [];
 
+	/** UTILIZADO DIRETAMENTE NA DIRETIVA NG-REPEAT. VALORES ATUALIZADOS PELA DIRETAIVA NG-MODEL */
+	$scope.filtro = '';
+
 	$http.get('v1/fotos').success(function(fotos) {
 		$scope.fotos = fotos
 	}).error(function(error) {
 		console.log(error)
 	});
-
-	/*var promise = $http.get('v1/fotos');
-	promise.then(function(retorno) {
-		$scope.fotos = retorno.data;
-	}).catch(function(error) {
-		console.log(error);
-	});*/
 
 });
